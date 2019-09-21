4bfc53a2e99a69021b21a7f1ab208f7ab61b37be
TIMOTHY P O'BRIEN <codemonkey.tim@gmail.com>
2019-09-21 06:12:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
+
